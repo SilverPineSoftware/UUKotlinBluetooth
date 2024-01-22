8f955b57c288aa1b32194ce904bbc65656cbdb97
Ryan DeVore <ryan@silverpine.com>
2024-01-22 07:46:42 -0800

    configure logging

--- app/src/main/java/com/silverpine/uu/sample/bluetooth/App.kt
+++ app/src/main/java/com/silverpine/uu/sample/bluetooth/App.kt
@@ -3,6 +3,8 @@ package com.silverpine.uu.sample.bluetooth
 import android.app.Application
 import com.silverpine.uu.bluetooth.UUBluetooth
 import com.silverpine.uu.core.UUResources
+import com.silverpine.uu.logging.UUConsoleLogger
+import com.silverpine.uu.logging.UULog
 import com.silverpine.uu.sample.bluetooth.ui.Strings
 
 class App: Application()
@@ -13,6 +15,7 @@ class App: Application()
 
         UUResources.init(applicationContext)
         UUBluetooth.init(applicationContext)
+        UULog.init(UUConsoleLogger())
         Strings.init(applicationContext)
     }
 }
